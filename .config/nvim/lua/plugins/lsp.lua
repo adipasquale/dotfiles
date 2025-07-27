7586ac1a5af2daab06179dcef89a27f64a8739d5
Adrien Di Pasquale <adrien@dipasquale.fr>
2025-07-27 12:36:47 +0200

    fix

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -7,6 +7,17 @@ return {
     opts = {},
   },
 
+  -- default configs for LSP
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- we skip Mason for ruby lsp and Rubocop because it’s installed through bundler and rbenv
+      -- cf https://github.com/mason-org/mason.nvim/issues/1777
+      vim.lsp.enable("ruby_lsp")
+      vim.lsp.enable("rubocop")
+    end,
+  },
+
   -- allows written config of LSP to install with mason
   {
     "williamboman/mason-lspconfig.nvim",
@@ -21,22 +32,17 @@ return {
           "lua_ls",
           "gh_actions_ls",
           "html",
-          "rubocop",
-          "ruby_lsp",
+          -- "rubocop",
+          -- "ruby_lsp",
           "ts_ls",
         },
         -- hack required to have ruby-lsp work with rbenv
-        ruby_lsp = function()
-          require("lspconfig").ruby_lsp.setup({
-            cmd = { os.getenv("HOME") .. "/.rbenv/shims/ruby-lsp" },
-          })
-        end,
+        -- ruby_lsp = function()
+        --   require("lspconfig").ruby_lsp.setup({
+        --     cmd = { os.getenv("HOME") .. "/.rbenv/shims/ruby-lsp" },
+        --   })
+        -- end,
       })
     end,
-  },
-
-  -- default configs for LSP
-  {
-    "neovim/nvim-lspconfig",
   },
 }
